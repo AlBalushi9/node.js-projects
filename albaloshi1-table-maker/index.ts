eae7ac8e2d9df125949321a39520650ac8b00d90
Albaloshi9 <userdied722@gmail.com>
2024-03-21 23:45:41 -0700

    first commit

---
+++ albaloshi1-table-maker/index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const table = await inquirer.prompt([
+    {
+        message: "Enter your table number >>",
+        type: "number",
+        name: "myNumber",
+    },
+]);
+let resul = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+resul.forEach((cur) => {
+    console.log(`${cur} x ${table.myNumber} = ${cur * table.myNumber}`);
+});
